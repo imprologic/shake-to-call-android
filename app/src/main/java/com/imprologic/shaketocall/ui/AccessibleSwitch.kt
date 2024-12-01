c3789f33849c728d4ceae558debbe28617a3d74b
imprologic <imprologic@protonmail.com>
2024-12-01 15:15:04 -0500

    main layout

--- app/src/main/java/com/imprologic/shaketocall/ui/AccessibleSwitch.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/AccessibleSwitch.kt
@@ -1,15 +1,11 @@
-package com.imprologic.shaketocall
+package com.imprologic.shaketocall.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
@@ -20,7 +16,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
