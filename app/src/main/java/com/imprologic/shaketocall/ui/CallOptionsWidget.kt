03f82dc0cd6d40c1694a9d371ad2f39f7f7cf46d
imprologic <imprologic@protonmail.com>
2024-12-01 17:14:26 -0500

    more UI

--- app/src/main/java/com/imprologic/shaketocall/ui/CallOptionsWidget.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/CallOptionsWidget.kt
@@ -1,23 +1,30 @@
 package com.imprologic.shaketocall.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+
+import com.imprologic.shaketocall.R
 
 
 @Composable
 fun CallOptionsWidget(modifier: Modifier) {
     OutlinedCard(
         modifier = modifier
-            .padding(16.dp)
     ) {
-        Text(
-            text = "Shake to Call",
-            modifier = Modifier
-                .padding(16.dp),
-        )
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = stringResource(id = R.string.shake_to_call),
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 16.dp)
+            )
+            AccessibleSwitch()
+        }
     }
 }
