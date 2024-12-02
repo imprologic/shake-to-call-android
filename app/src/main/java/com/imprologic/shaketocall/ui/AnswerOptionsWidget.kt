5f674b59ae9fcb687f95ae35e53a5f3031356cb7
imprologic <imprologic@protonmail.com>
2024-12-01 19:26:32 -0500

    foreground service attempt

--- app/src/main/java/com/imprologic/shaketocall/ui/AnswerOptionsWidget.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/AnswerOptionsWidget.kt
@@ -2,18 +2,23 @@ package com.imprologic.shaketocall.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.imprologic.shaketocall.R
+import com.imprologic.shaketocall.services.MonitoringServiceStarter
 
 
 @Composable
 fun AnswerOptionsWidget(modifier: Modifier) {
+    val context = LocalContext.current
+
     OutlinedCard(
         modifier = modifier
     ) {
@@ -24,7 +29,13 @@ fun AnswerOptionsWidget(modifier: Modifier) {
                 modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 16.dp),
             )
             AccessibleSwitch()
-
+            Button(
+                onClick = {
+                    MonitoringServiceStarter.startService(context)
+                }
+            ) {
+                Text(text = "launch service")
+            }
         }
     }
 }
