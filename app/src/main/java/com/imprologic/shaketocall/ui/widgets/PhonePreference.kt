8ac4c931746ce11b5ee346203fb45433cfb3b142
imprologic <imprologic@protonmail.com>
2024-12-28 18:18:40 -0500

    WIP commit

--- app/src/main/java/com/imprologic/shaketocall/ui/widgets/PhonePreference.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/widgets/PhonePreference.kt
@@ -1,5 +1,6 @@
 package com.imprologic.shaketocall.ui.widgets
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,25 +9,39 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.imprologic.shaketocall.R
+
 
 @Composable
 fun PhonePreference(
     title: String,
-    value: String,
-    onValueChange: (String) -> Unit
+    subtitle: String,
+    value: String?,
+    onValueChange: (String?) -> Unit
 ) {
-    val state = remember { mutableStateOf(value) }
+    val subtitleState = remember { mutableStateOf(subtitle) }
+    val dialogState = remember { mutableStateOf(false) }
 
     Row(
         modifier = Modifier
@@ -35,14 +50,18 @@ fun PhonePreference(
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Column(
-            modifier = Modifier.weight(1f)
+            modifier = Modifier
+                .weight(1f)
+                .clickable(
+                    onClick = { dialogState.value = !dialogState.value }
+                )
         ) {
             Text(
                 text = title,
                 style = MaterialTheme.typography.bodyLarge,
             )
             Text(
-                text = state.value,
+                text = subtitleState.value,
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onSurfaceVariant,
             )
@@ -57,6 +76,73 @@ fun PhonePreference(
                 modifier = Modifier.size(72.dp)
             )
         }
+        when {
+            dialogState.value ->
+                AlertDialogExample(
+                    value = value,
+                    onDismissRequest = { dialogState.value = false },
+                    onConfirmation = { result ->
+                        dialogState.value = false
+                        onValueChange(result)
+                    }
+                )
+        }
     }
 
+}
+
+
+@Composable
+fun AlertDialogExample(
+    value: String?,
+    onDismissRequest: () -> Unit,
+    onConfirmation: (result: String?) -> Unit,
+) {
+    val resultState = remember { mutableStateOf(value) }
+
+    Dialog(
+        onDismissRequest = { onDismissRequest() }
+    ) {
+        Card(
+            shape = RoundedCornerShape(16.dp),
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = stringResource(R.string.alert_number_to_call),
+                    modifier = Modifier.padding(16.dp),
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                TextField(
+                    value = resultState.value ?: "",
+                    onValueChange = {
+                        resultState.value = it
+                    },
+                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone),
+                    textStyle = MaterialTheme.typography.bodyLarge.copy(fontSize = 20.sp),
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    TextButton(
+                        onClick = { onDismissRequest() },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Dismiss")
+                    }
+                    TextButton(
+                        onClick = { onConfirmation( resultState.value ) },
+                        modifier = Modifier.padding(8.dp),
+                    ) {
+                        Text("Confirm")
+                    }
+                }
+            }
+        }
+    }
 }
