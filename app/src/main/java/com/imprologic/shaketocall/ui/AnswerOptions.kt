d4522b83ef29c43e713c3388a7d3470697eac3e5
imprologic <imprologic@protonmail.com>
2024-12-29 17:51:54 -0500

    hoisting some states

--- app/src/main/java/com/imprologic/shaketocall/ui/AnswerOptions.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/AnswerOptions.kt
@@ -3,6 +3,8 @@ package com.imprologic.shaketocall.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -16,13 +18,15 @@ import com.imprologic.shaketocall.ui.widgets.SwitchPreference
 fun AnswerOptions() {
     val context = LocalContext.current
     val settingsManager = SettingsManager(context)
+    val shakeToAnswerState = remember { mutableStateOf(settingsManager.shakeToAnswer) }
 
     Column() {
         SwitchPreference (
             title = stringResource(R.string.shake_to_answer),
             subtitle = stringResource(R.string.shake_to_answer_description),
-            value = settingsManager.shakeToAnswer,
+            value = shakeToAnswerState.value,
             onValueChange = {
+                shakeToAnswerState.value = it
                 settingsManager.shakeToAnswer = it
                 MonitoringServiceStarter.manageService(context)
             }
--- app/src/main/java/com/imprologic/shaketocall/ui/widgets/SwitchPreference.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/widgets/SwitchPreference.kt
@@ -3,8 +3,6 @@ package com.imprologic.shaketocall.ui.widgets
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -15,8 +13,6 @@ fun SwitchPreference(
     value: Boolean,
     onValueChange: (Boolean) -> Unit
 ) {
-    val state = remember { mutableStateOf(value) }
-
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -40,9 +36,8 @@ fun SwitchPreference(
         }
         Spacer(modifier = Modifier.width(8.dp))
         Switch(
-            checked = state.value,
+            checked = value,
             onCheckedChange = {
-                state.value = it
                 onValueChange(it)
             }
         )
