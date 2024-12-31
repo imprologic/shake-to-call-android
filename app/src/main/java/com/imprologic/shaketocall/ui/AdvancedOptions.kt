c4d68121b489016b5cd6cf4f0a0ac717e57a79d6
imprologic <imprologic@protonmail.com>
2024-12-30 20:18:01 -0500

    shake magnitude

--- app/src/main/java/com/imprologic/shaketocall/ui/AdvancedOptions.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/AdvancedOptions.kt
@@ -1,8 +1,7 @@
 package com.imprologic.shaketocall.ui
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -11,13 +10,13 @@ import com.imprologic.shaketocall.services.MonitoringServiceStarter
 import com.imprologic.shaketocall.services.SettingsManager
 import com.imprologic.shaketocall.ui.widgets.PreferenceSection
 import com.imprologic.shaketocall.ui.widgets.SliderPreference
-import com.imprologic.shaketocall.ui.widgets.SwitchPreference
 
 
 @Composable
 fun AdvancedOptions() {
     val context = LocalContext.current
     val settingsManager = SettingsManager(context)
+    val shakeMagnitudeState = remember { mutableFloatStateOf(settingsManager.shakeMagnitude) }
 
     PreferenceSection(
         title = stringResource(R.string.advanced_options),
@@ -25,8 +24,13 @@ fun AdvancedOptions() {
         SliderPreference(
             title = stringResource(R.string.shake_magnitude),
             metric = stringResource(R.string.shake_metric),
-            value = 12f,
-            onValueChange = {}
+            value = shakeMagnitudeState.floatValue,
+            onValueChange = {
+                shakeMagnitudeState.floatValue = it
+                settingsManager.shakeMagnitude = it
+                MonitoringServiceStarter.restartService(context)
+            },
+            valueRange = 10f.rangeTo(20f)
         )
     }
 
