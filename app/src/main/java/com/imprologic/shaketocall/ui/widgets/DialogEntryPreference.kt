797be3b7f0c7cfc67d089948b7d2669398f8e4f2
imprologic <imprologic@protonmail.com>
2025-01-01 14:54:54 -0500

    generic dialog preference

--- app/src/main/java/com/imprologic/shaketocall/ui/widgets/DialogEntryPreference.kt
+++ app/src/main/java/com/imprologic/shaketocall/ui/widgets/DialogEntryPreference.kt
@@ -6,22 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
 
 
 @Composable
@@ -65,7 +57,7 @@ fun DialogEntryPreference(
         }
         when {
             dialogState.value ->
-                GenericDialog(
+                PhoneEntryDialog(
                     title = dialogTitle,
                     value = value,
                     keyboardOptions = keyboardOptions,
@@ -82,60 +74,3 @@ fun DialogEntryPreference(
 
 }
 
-
-@Composable
-fun GenericDialog(
-    title: String,
-    value: String?,
-    keyboardOptions: KeyboardOptions,
-    onDismissRequest: () -> Unit,
-    onConfirmation: (result: String?) -> Unit,
-) {
-    val resultState = remember { mutableStateOf(value) }
-
-    Dialog(
-        onDismissRequest = { onDismissRequest() }
-    ) {
-        Card(
-            shape = RoundedCornerShape(16.dp),
-        ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(
-                    text = title,
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.titleMedium,
-                )
-                TextField(
-                    value = resultState.value ?: "",
-                    onValueChange = {
-                        resultState.value = it
-                    },
-                    keyboardOptions = keyboardOptions,
-                    textStyle = MaterialTheme.typography.bodyLarge.copy(fontSize = 20.sp),
-                    modifier = Modifier.padding(bottom = 16.dp)
-                )
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    TextButton(
-                        onClick = { onDismissRequest() },
-                        modifier = Modifier.padding(8.dp),
-                    ) {
-                        Text(stringResource(android.R.string.cancel))
-                    }
-                    TextButton(
-                        onClick = { onConfirmation( resultState.value ) },
-                        modifier = Modifier.padding(8.dp),
-                    ) {
-                        Text(stringResource(android.R.string.ok))
-                    }
-                }
-            }
-        }
-    }
-}
