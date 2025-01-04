39cd8375d496f9cfb56c02cd4298cd3e725eb7a9
imprologic <imprologic@protonmail.com>
2025-01-04 17:13:42 -0500

    fixed bad rebase

--- app/src/main/java/com/imprologic/shaketocall/services/MonitoringService.kt
+++ app/src/main/java/com/imprologic/shaketocall/services/MonitoringService.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -15,23 +16,30 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.IBinder
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.telecom.TelecomManager
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import com.imprologic.shaketocall.MainActivity
+import com.imprologic.shaketocall.R
 import kotlin.math.sqrt
 
+private const val channelName = "shake_service_channel"
 
 class MonitoringService : Service(), SensorEventListener {
 
     val tag = "MonitoringService"
 
-    private var shakeThreshold = 12.0f  // TODO: get this from Settings
+    private var shakeThreshold = SettingsManager.DEFAULT_SHAKE_MAGNITUDE
+    private var zAxisFactor = SettingsManager.DEFAULT_Z_AXIS_FACTOR
 
     private lateinit var sensorManager: SensorManager
     private lateinit var telephonyManager: TelephonyManager
+    private lateinit var settingsManager: SettingsManager
     private var accelerometer: Sensor? = null
     private val shakeStateMachine = ShakeStateMachine(::actOnShake)
 
@@ -41,9 +49,12 @@ class MonitoringService : Service(), SensorEventListener {
     override fun onCreate() {
         super.onCreate()
         Log.i(tag, "onCreate")
-        createNotificationChannel()
+        createMainNotificationChannel()
         startForeground(1, createNotification())
-        Log.d("ShakeService", "Service started")
+        Log.d(tag, "Service started")
+        settingsManager = SettingsManager(this)
+        shakeThreshold = settingsManager.shakeMagnitude
+        zAxisFactor = settingsManager.zAxisFactor
         // Initialize shake detection and telephony handling here
         sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
@@ -74,11 +85,11 @@ class MonitoringService : Service(), SensorEventListener {
         return null
     }
 
-    private fun createNotificationChannel() {
+    private fun createMainNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             Log.i(tag, "createNotificationChannel")
             val channel = NotificationChannel(
-                "shake_service_channel",
+                channelName,
                 "Shake Detection Service",
                 NotificationManager.IMPORTANCE_HIGH
             )
@@ -87,14 +98,25 @@ class MonitoringService : Service(), SensorEventListener {
         }
     }
 
+
     private fun createNotification(): Notification {
         Log.i(tag, "createNotification")
-        return NotificationCompat.Builder(this, "shake_service_channel")
-            .setContentTitle("Shake Detection Running")
-            .setContentText("Monitoring for shake events and calls")
-            .setSmallIcon(android.R.drawable.ic_menu_info_details)
+        // Create an Intent to open MainActivity
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        // Create a PendingIntent for the Intent
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+        // Create the notification
+        return NotificationCompat.Builder(this, channelName)
+            .setContentTitle(this.getString(R.string.service_notification_title))
+            .setContentText(this.getString(R.string.service_notification_content))
+            .setSmallIcon(R.drawable.ic_stat_monitoring_service)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setOngoing(true)
+            .setContentIntent(pendingIntent)
             .build()
     }
 
@@ -107,7 +129,7 @@ class MonitoringService : Service(), SensorEventListener {
             val y = it.values[1]
             val z = it.values[2]
 
-            val shakeMagnitude = sqrt((x * x + y * y + z * z).toDouble()).toFloat()
+            val shakeMagnitude = sqrt(x * x + y * y + zAxisFactor * z * z)
 
             if (shakeMagnitude > shakeThreshold) {
                 shakeStateMachine.handleShakeEvent()
@@ -117,7 +139,7 @@ class MonitoringService : Service(), SensorEventListener {
 
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-//        TODO("Not yet implemented")
+        // Not needed
     }
 
 
@@ -147,6 +169,10 @@ class MonitoringService : Service(), SensorEventListener {
     // Make call
 
     private fun makeCall() {
+        if (!settingsManager.shakeToCall) {
+            Log.i(tag, "shakeToCall option is disabled")
+            return
+        }
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.CALL_PHONE
@@ -155,8 +181,13 @@ class MonitoringService : Service(), SensorEventListener {
             Log.i(tag, "CALL_PHONE permission denied")
             return
         }
-        val phoneToCall = SettingsManager(this).defaultPhone
+        val phoneToCall = settingsManager.defaultPhone
+        if (phoneToCall == null || phoneToCall.length == 0) {
+            Log.e(tag, "Phone number is empty")
+            return
+        }
         Log.i(tag, "Will call $phoneToCall")
+        notifyShakeConfirmed()
         val telecomManager = getSystemService(TELECOM_SERVICE) as TelecomManager
         val uri = Uri.fromParts("tel", phoneToCall, null)
         val bundle = Bundle()
@@ -168,6 +199,11 @@ class MonitoringService : Service(), SensorEventListener {
     // End call
 
     private fun endCall() {
+        if (!settingsManager.shakeToHangUp) {
+            // TODO: Should we have a different setting here?
+            Log.i(tag, "shakeToHangUp option is disabled")
+            return
+        }
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
             Log.e(tag, "Call ending not supported for version ${Build.VERSION.SDK_INT}")
             return
@@ -180,6 +216,7 @@ class MonitoringService : Service(), SensorEventListener {
             Log.e(tag, "READ_PHONE_STATE permission not granted, cannot answer call")
             return
         }
+        notifyShakeConfirmed()
         val telecomManager = getSystemService(TELECOM_SERVICE) as TelecomManager
         if (telecomManager.isInCall) {
             telecomManager.endCall()
@@ -189,6 +226,10 @@ class MonitoringService : Service(), SensorEventListener {
     // Answer call
 
     private fun answerCall() {
+        if (!settingsManager.shakeToAnswer) {
+            Log.i(tag, "shakeToAnswer option is disabled")
+            return
+        }
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             Log.e(tag, "Call answering not supported for version ${Build.VERSION.SDK_INT}")
             return
@@ -201,11 +242,27 @@ class MonitoringService : Service(), SensorEventListener {
             Log.e(tag, "READ_PHONE_STATE permission not granted, cannot answer call")
             return
         }
+        notifyShakeConfirmed()
         val telecomManager = getSystemService(TELECOM_SERVICE) as TelecomManager
         if (telecomManager.isInCall) {
             telecomManager.acceptRingingCall()
         }
     }
 
+
+    private fun notifyShakeConfirmed() {
+        try {
+            val vibrator = getSystemService(VIBRATOR_SERVICE) as Vibrator
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val effect = VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE)
+                vibrator.vibrate(effect)
+            } else {
+                @Suppress("DEPRECATION")
+                vibrator.vibrate(500)
+            }
+        } catch (e: Error) {
+           Log.w(tag, "Unable to vibrate", e)
+        }
+    }
 
 }
