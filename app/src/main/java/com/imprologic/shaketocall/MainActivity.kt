556460e846849a4a27068ba91091b4e35ed9bc8f
imprologic <imprologic@protonmail.com>
2025-08-15 16:24:18 -0400

    enabled edge-to-edge

--- app/src/main/java/com/imprologic/shaketocall/MainActivity.kt
+++ app/src/main/java/com/imprologic/shaketocall/MainActivity.kt
@@ -3,6 +3,7 @@ package com.imprologic.shaketocall
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -31,6 +32,8 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             AppContent()
         }
